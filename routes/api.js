44162f965a0d3111c9500b85383e74976278bf12
niuniuanran <irene.anran.niu@gmail.com>
2020-07-05 20:19:40 +1200

    Implement api route for unit conversion

--- routes/api.js
+++ routes/api.js
@@ -12,19 +12,30 @@ var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  var convertHandler = new ConvertHandler();
 
-  app.route('/api/convert')
-    .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
-    });
-    
+    var convertHandler = new ConvertHandler();
+
+    app.route('/api/convert')
+        .get(function (req, res) {
+            var input = req.query.input;
+            var initNum = convertHandler.getNum(input);
+            var initUnit = convertHandler.getUnit(input);
+            if (!initNum || !initUnit) {
+                res.status(400);
+                const errorMessage = initNum ? "invalid unit" : (initUnit ? "invalid number" : "invalid number and unit")
+                res.json({"err": errorMessage})
+            }
+            var returnNum = convertHandler.convert(initNum, initUnit);
+            var returnUnit = convertHandler.getReturnUnit(initUnit);
+            var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+            //{initNum: 3.1, initUnit: 'mi', returnNum: 5.0000008, returnUnit: 'km', string: '3.1 miles converts to 5.00002 kilometers'}
+            res.json({
+                initNum: initNum,
+                initUnit: initUnit,
+                returnNum: returnNum,
+                returnUnit: returnUnit,
+                string: toString
+            });
+        });
+
 };
